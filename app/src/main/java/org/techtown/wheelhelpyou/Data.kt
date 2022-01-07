7264a8f936c5bc6e26f650d5122acaff0db72240
hihi-git <20201111@duksung.ac.kr>
2022-01-07 15:03:06 +0900

    맵스 위치(1)

---
+++ app/src/main/java/org/techtown/wheelhelpyou/Data.kt
@@ -0,0 +1,7 @@
+package org.techtown.wheelhelpyou
+
+import com.google.android.gms.maps.model.LatLng
+
+data class Data(
+    val name: String = "",
+    val latLng: LatLng)
--- app/src/main/java/org/techtown/wheelhelpyou/MapsActivity.kt
+++ app/src/main/java/org/techtown/wheelhelpyou/MapsActivity.kt
@@ -9,9 +9,12 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.techtown.wheelhelpyou.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
@@ -53,6 +56,7 @@ GoogleMap.OnMyLocationClickListener{
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        //권한 확인
         if (checkCallingPermission(Manifest.permission.ACCESS_FINE_LOCATION) !=
             PackageManager.PERMISSION_GRANTED &&
             checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) !=
@@ -69,7 +73,25 @@ GoogleMap.OnMyLocationClickListener{
         mMap.setOnMyLocationButtonClickListener(this)
         mMap.setOnMyLocationClickListener(this)
 
-    }
+        // 위치 담을 list 리스트 설정
+        val list = arrayListOf<Data>()
+        // list에 Data 클래스 (이름, 위도/경도) 넣기
+        list.add(Data("덕성여대",LatLng( 37.65137716381777, 127.01612821102516)))
+        list.add(Data("효문고등학교",LatLng(37.653123393883156, 127.01836458404172)))
+
+        // 반복문 통해 list의 좌표값 뽑아서 마커 설정하기
+        for(i in 0..list.size-1) {
+            val markerOption = MarkerOptions()
+            markerOption.position(list[i].latLng).title(list[i].name)
+            mMap.addMarker(markerOption)
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(list[0].latLng))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15F))
+
+
+
+   }
 
     override fun onMyLocationButtonClick(): Boolean {
         Toast.makeText(this,"MyLocation clicked", Toast.LENGTH_SHORT).show();
